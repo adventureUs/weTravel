4db5f12704713912924e0866724918dcdaf7fa06
TinaHeiligers <tina.heiligers@gmail.com>
2017-05-03 14:39:38 -0400

    couldn't get bootstrap theme working, created own stylesheet

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,24 +3,11 @@ import { Link } from 'react-router'
 
 const Navbar = () =>
   (<div>
-    <nav className="navbar navbar-default">
-      <div className="container-fluid">
-        <div className="navbar-header">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-        </div>
-
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
-            <Link to="/" className="navbar-brand">Title</Link>
-            <Link to="/travel_buddies" className="navbar-brand">Travel Buddies</Link>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    </div>
+     <ul>
+    <Link to="/" className='navbar navbar-title'>Interpolated Title for Trip Name</Link>
+    <Link to="/travel-buddies" className='navbar navbar-title'>Travel Buddies</Link>
+</ul>
+  </div>
   )
 
 export default Navbar
