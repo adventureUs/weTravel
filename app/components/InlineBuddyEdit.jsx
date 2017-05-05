430dc3428d61365fbbe55b7786259d6696e81a40
TinaHeiligers <tina.heiligers@gmail.com>
2017-05-05 14:42:15 -0400

    switching

---
+++ app/components/InlineBuddyEdit.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from 'react'
+import { RIEToggle, RIEInput, RIETextArea, REINumber, RIETags, RIESelect } from 'riek'
+import {Route} from 'react-router'
+import DatePicker from 'react-datepicker'
+
+import moment from 'moment'
+import firebase from 'APP/fire'
+const auth = firebase.auth()
+const db = firebase.database
+
+export default class InlineBuddyEdit extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name:'Please enter name here',
+      email: auth.currentUser ? auth.currentUser.email : 'no email',
+      status: {id: "1", text: "Invited"},
+      statusOptions: [
+        {id: "1", text: "Invited"},
+        {id: "2", text: "Going"},
+        {id: "3", text: "Can\'t make it"}
+        ],
+      homeBase:'New York',
+      startDate: moment(),
+      endDate: moment()
+    }
+
+    // this.handleChange = this.handleChange.bind(this)
+    // this.handleChangeStart = this.handleChangeStart.bind(this)
+    // this.handleChangeEnd = this.handleChangeEnd.bind(this)
+    // this.showDate = this.showDate.bind(this)
+  }
+
+  handleChange = e => {
+    console.log('TravelBuddies on Change', e.target.name, e.target.value)
+  }
+
+
+  handleChangeStart = m => {
+    this.setState({startDate: m})
+    console.log('TravelBuddies Start Date On Change', this.state.startDate.format('MMM Do YY'))
+  }
+
+  handleChangeEnd = m => {
+    this.setState({endDate: m})
+    console.log('TravelBuddies End Date On Change', this.state.startDate.format('MMM Do YY'))
+  }
+  showDate = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+  virtualServerCallback = (newState) => {
+    this.setState(newState)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>BUDDIES</h1>
+        <div className="container">
+          <div className="form-horizontal">
+            <div className="col-md-3">
+            <span>Name: </span>
+              <RIEInput
+                value={this.state.name}
+                change={this.virtualServerCallback}
+                propName="name"
+                className={this.state.highlight ? "editable" : ""}
+                validate={this.isStringAcceptable}
+                classLoading="loading"
+                classInvalid="Invalid"/>
+            </div>
+            <div className="col-md-3">
+            <span>Email: </span>
+              <RIEInput
+                value={this.state.email}
+                change={this.virtualServerCallback}
+                propName="email"
+                className={this.state.highlight ? "editable" : ""}
+                validate={this.isStringAcceptable}
+                classLoading="loading"
+                classInvalid="Invalid"/>
+            </div>
+            <div className="col-md-3">
+            <span>Status: </span>
+              <RIESelect
+              value={this.state.status}
+              className={this.state.highlight ? "editable" : ""}
+              options={this.state.statusOptions}
+              change={this.virtualServerCallback}
+              classLoading="loading"
+              propName="status" />
+            </div>
+            <div className="col-md-3">
+            <span>Home Base: </span>
+              <RIEInput
+                value={this.state.homeBase}
+                change={this.virtualServerCallback}
+                propName="homeBase"
+                className={this.state.highlight ? "editable" : ""}
+                validate={this.isStringAcceptable}
+                classLoading="loading"
+                classInvalid="Invalid"/>
+            </div>
+            <div className="col-md-3">
+            <span>Availability Start Date: </span>
+              <DatePicker
+                  selected={this.state.startDate}
+                  selectsStart
+                  startDate={this.state.startDate}
+                  endDate={this.state.endDate}
+                  onChange={this.handleChangeStart}
+              />
+            </div>
+            <div className="col-md-3">
+            <span>Availability End Date: </span>
+              <DatePicker
+                  selected={this.state.endDate}
+                  selectsEnd
+                  startDate={this.state.startDate}
+                  endDate={this.state.endDate}
+                  onChange={this.handleChangeEnd}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
