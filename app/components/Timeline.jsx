59d30398dd2cdde3fbbc2ac98b0c183f526111d0
TinaHeiligers <tina.heiligers@gmail.com>
2017-05-09 17:24:52 -0400

    have items console logging out on click, select and resizing

--- app/components/Timeline.jsx
+++ app/components/Timeline.jsx
@@ -14,7 +14,7 @@ const groups = [
 // user name, user startDate for this trip, user endDate for this trip (currentTripUserStartDate, currentTripUserEndDate)
 const items = [
   {id: 'dlxw3BoFWJMfMpGoBlRhQsRAiMB2', group: 'dlxw3BoFWJMfMpGoBlRhQsRAiMB2', title: 'Tina', start_time: moment('Tue May 12 2017 12:51:11 GMT-0400'), end_time: moment('Tue May 16 2017 13:00:11 GMT-0400')},
-  {id: 'n2JCRgwSVuPr1WzifDLia2Ti8gr1', group: 'n2JCRgwSVuPr1WzifDLia2Ti8gr1', title: 'Allison', start_time: moment('Tue May 11 2017 12:51:11 GMT-0400'), end_time: moment('Tue May 14 2017 13:00:11 GMT-0400')}
+  {id: 'n2JCRgwSVuPr1WzifDLia2Ti8gr1', group: 'n2JCRgwSVuPr1WzifDLia2Ti8gr1', title: 'Allison', start_time: moment('Tue May 11 2017 12:51:11 GMT-0400'), end_time: moment('Tue June 14 2017 13:00:11 GMT-0400')}
 ]
 
 const db = firebase.database()
@@ -34,21 +34,33 @@ export default class extends Component {
   }
 
   findMinStartDate = (items) => {
+    // takes start dates from each buddy and returns the min of these minus 1 day in unix number form casts from 1970 ('* 1000')
     let tempMinDate = items.map(item => item.start_time)
     .reduce((minDate, dateMoment) => { return dateMoment < minDate ? dateMoment : minDate }, moment().add(10, 'years'))
-    console.log('MIN DATE', tempMinDate)
-    return tempMinDate
+    let renderMinStartDate = moment(tempMinDate).add(-1, 'days')
+    return renderMinStartDate.unix()*1000
   }
 
   findMaxEndDate = (items) => {
+    // takes end dates from each buddy and returns the max of these minus 1 day in unix number form and casts from 1970 ('* 1000')
     let tempMaxDate = items.map(item => item.end_time)
     .reduce((maxDate, dateMoment) => { return dateMoment > maxDate ? dateMoment : maxDate }, moment())
-    console.log('MAX DATE', tempMaxDate)
-    return tempMaxDate
+    let renderMaxEndDate = moment(tempMaxDate).add(1, 'days')
+    return renderMaxEndDate.unix()*1000
   }
-
+  onItemClick = (itemId, e) => {
+    // returns the id of the item selected
+    console.log('ON ITEM CLICK HANDLER, itemId: ', itemId, 'e: ', e)
+  }
+  onItemSelect = (itemId, e) => {
+    // returns the id of the item selected
+    console.log('ON ITEM SELECT HANDLER, itemId: ', itemId, 'e: ', e)
+  }
+  onItemResize = (itemId,time,edge) => {
+    console.log('ON ITEM RESIZE HANDLER, itemId: ', itemId, 'time: ', time, 'edge: ', edge)
+    // we get back the itemId, the time changed to in unix number format and the edge that was changed
+  }
   render() {
-
 // This object sets the untis on the timeline.
 // Currently, it is set to display days, months and years
     const timeSteps = {
@@ -71,6 +83,9 @@ export default class extends Component {
             visibleTimeStart={this.findMinStartDate(items)}
             visibleTimeEnd={this.findMaxEndDate(items)}
             sidebarWidth={70}
+            onItemClick={this.onItemClick}
+            onItemSelect={this.onItemSelect}
+            onItemResize={this.onItemResize}
             />
       </div>
     )
