4aad890a980b4cf0b10e66be026f981fcb235c34
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-05-12 16:22:50 -0400

    fleshed out code for rending Ideas

--- app/components/TimelineIndex.js
+++ app/components/TimelineIndex.js
@@ -9,7 +9,7 @@ import moment from 'moment'
 // user name, user startDate for this trip, user endDate for this trip (currentTripUserStartDate, currentTripUserEndDate)
 // loop over buddies in trip and grab info
 // From trip (ie. tripRef) we need: buddiesid -> id, buddiesid -> group, availabilityStart -> start_time, availabilityEnd -> end_time
-// Note: title in an sliderData group will be the name of a buddy or self
+// Note: title in an bodyData group will be the name of a buddy or self
 
 // BUGS IN HERE:  DID MOUNT AND SO ALSO LISTEN FOR SLIDERS DO NOT TRIGGER AGAIN ONCE USERID IS IN PROPS, SO CANRESIZE IS NEVER BECOMING TURE;  SIMILARLY SUSPECT THAT CHANGES IN BUDDIES WON"T RERENDER IN THE TIMELINE.
 
@@ -23,51 +23,75 @@ export default class TimelineIndex extends React.Component {
     this.listenForSliders = this.listenForSliders.bind(this)
   }
   componentWillUnmount() {
-    // When we unmount, stop listening.
     this.unsubscribe()
   }
   componentDidMount() {
     // console.log('ENTERED COMPONENENT-DID-MOUNT PROPS', this.props)
-    this.listenForSliders(this.props.tripRef, this.props.userId)
+    this.props.whichTab && this.props.whichTab === 'Buddies' ?
+      this.listenFor('buddies') // For Buddies Slider Timelne View
+      :
+      this.listenFor('ideas') // For Ideas Box Timeline View
   }
-  // For mysteriosu reasons,  DidMount works but WillReceiveProps below did not .... Learning lesson??
-  // componentWilReceiveProps(incoming, outgoing) {
-  //   console.log('ENTERED COMPONENENT-WILL-RECEIVE-PROPS')
-  //   this.listenForSliders(incoming.tripRef, incoming.userId)
-  // }
-  listenForSliders(tripRef, userId) {
+  /* For mysteriosu reasons,  DidMount works but WillReceiveProps below did not .... Learning lesson?? */
+// componentWilReceiveProps(incoming, outgoing) {
+//   console.log('ENTERED COMPONENENT-WILL-RECEIVE-PROPS')
+//   this.listenForSliders(incoming.tripRef, incoming.userId)
+// }
+  listenFor(branch) {
     if (this.unsubscribe) this.unsubscribe()
-    // LOGIC HERE BEACUSE FOR NOW WE ONLY RENDER A SLIDER TIMELINE:
-    // Getting data from trip part of db
-    let sliderData = [], namesData = []
-    // console.log('TIMELINE_INDEX, OUTSIDE LISTENER USERID:', this.props.userId, userId)
-    const listener = tripRef.on('value', snapshot => {
-      const buddiesObject = snapshot.val().buddies
-      const buddiesIds = Object.keys(buddiesObject) // do not need this line of code
-      // now map over the buddies Ids and grab the start and end dates
-      sliderData = Object.keys(buddiesObject).map(key => {
-        // console.log('TIMELINE_INDEX, LISTENER USERID: ', userId)
-        return {
-          id: key,
-          group: key,
-          title: buddiesObject[key].status.text,
-          start_time: moment(buddiesObject[key].startDate),
-          end_time: moment(buddiesObject[key].endDate),
-          canResize: key === userId ? 'both' : false,
-          canChangeGroup: false // if we oneday get to items do conditional checks for item categories here
-        }
-      })
-      namesData = Object.keys(buddiesObject).map((key) =>
+    // Getting data from branch part of db
+    let bodyData = [], namesData = []
+    console.log('TIMELINE_INDEX, LISTEN_FOR', branch)
+    const listener = this.props.tripRef.on('value', snapshot => {
+      const dbObject = snapshot.val()[branch]
+      const branchIds = Object.keys(dbObject)
+      // now map over the db Ids and grab info as necessary depending
+      // on the branch typs.
+      if (branch === 'Buddies') {
+        bodyData = branchIds.map(key => {
+          // console.log('TIMELINE_INDEX, LISTENER USERID: ', userId)
+          return {
+            id: key,
+            group: key,
+            title: dbObject[key].status.text,
+            start_time: moment(dbObject[key].startDate),
+            end_time: moment(dbObject[key].endDate),
+            canResize: key === this.props.userId ? 'both' : false,
+            canChangeGroup: false // if we oneday get to items do conditional checks for item categories here
+          }
+        })
+        namesData = Object.keys(dbObject).map((key) =>
          ({
            id: key,
-           title: buddiesObject[key].name
+           title: dbObject[key].name
          })
-      )
-      // console.log('TIMELINE_INDEX LISTEN-FOR-SLIDERS, names, slider', namesData, sliderData)
+          )
+      } else { // Item Timeline Data
+        bodyData = branchIds.map(key => {
+        // console.log('TIMELINE_INDEX, LISTENER USERID: ', userId)
+          return {
+            id: key,
+            group: dbObject[key].category.id,
+            title: dbObject[key].ideaName,
+            start_time: moment(dbObject[key].startDate),
+            end_time: moment(dbObject[key].endDate),
+            canResize: dbObject[key].addedBy === this.props.userId ? 'both' : false,
+            canChangeGroup: false // if we oneday get to items do conditional checks for item categories here
+          }
+        })
+        namesData = Object.keys(dbObject).map((key) =>
+         ({
+           id: key,
+           title: dbObject[key].category.text
+         })
+          )
+      }
+
+      // console.log('TIMELINE_INDEX LISTEN-FOR-SLIDERS, names, body', namesData, bodyData)
       this.setState({groups: namesData})
-      this.setState({items: sliderData})
+      this.setState({items: bodyData})
     })
-    this.unsubscribe = () => tripRef.off('value', listener)
+    this.unsubscribe = () => this.props.tripRef.off('value', listener)
   }
 
   render() {
