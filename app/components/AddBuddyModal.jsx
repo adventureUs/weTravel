64548371bfc09468c3f2a5d763899511374a6547
TinaHeiligers <tina.heiligers@gmail.com>
2017-05-15 17:33:20 -0400

    saved data

--- app/components/AddBuddyModal.jsx
+++ app/components/AddBuddyModal.jsx
@@ -3,6 +3,11 @@ import React, { Component } from 'react'
 export default class AddBuddyModal extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      buddies: {}, // there's nothing on state when we go to the buddies tab
+      clipboard: `https://tern-2b37d.firebaseapp.com${window.location.pathname}`,
+      copied: ''
+    }
 
   }
   render() {
