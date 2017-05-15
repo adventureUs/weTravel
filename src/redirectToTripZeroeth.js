bd2c139bdfac19656c1dffd2df34ec4f07d69ff7
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-05-15 13:22:37 -0400

    refactor and everified initialRenderHack still works

--- src/redirectToTripZeroeth.js
+++ src/redirectToTripZeroeth.js
@@ -8,15 +8,15 @@ import redirectToTripZeroeth from 'APP/src/redirectToTripZeroeth'
 
 export default function redirectToTripZeroeth(userId) {
   firebase.database().ref('users')
-        .child(userId)
-        .child('trips')
-        .once('value')
-        .then(snapshot => {
-          const tripsArr = snapshot.val()
-          if (tripsArr.length === 1) {
-            return tripsArr[0]
-          }
-        })
-        .then((tripId) => browserHistory.push('/dashboard/' + tripId))
-        .catch(err => console.error(err))
+  .child(userId)
+  .child('trips')
+  .once('value')
+  .then(snapshot => {
+    const tripsArr = snapshot.val()
+    if (tripsArr.length === 1) {
+      return tripsArr[0]
+    }
+  })
+  .then((tripId) => browserHistory.push('/dashboard/' + tripId))
+  .catch(err => console.error(err))
 }
