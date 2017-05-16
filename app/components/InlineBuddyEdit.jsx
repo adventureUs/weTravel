a2af5773340d99ddc5d16185de188ee22c306498
Allison Alexander <aveleber@hotmailcom>
2017-05-16 11:00:53 -0400

    formatting edit user info modal

--- app/components/InlineBuddyEdit.jsx
+++ app/components/InlineBuddyEdit.jsx
@@ -25,15 +25,16 @@ export default class InlineBuddyEdit extends Component {
       userId: props.userId,
       name: 'Please enter your name here',
       email: 'no email',
-      status: { id: '1', text: 'Invited' },
-      statusOptions: [
-        { id: '1', text: 'Invited' },
-        { id: '2', text: 'Going' },
-        { id: '3', text: 'Can\'t make it' }
-      ],
+      status: 'Invited',
+      // statusOptions: [
+      //   { id: '1', text: 'Invited' },
+      //   { id: '2', text: 'Going' },
+      //   { id: '3', text: 'Can\'t make it' }
+      // ],
       homeBase: 'Please enter your city',
       startDate: '',
       endDate: '',
+
     }
   }
 
@@ -73,14 +74,36 @@ export default class InlineBuddyEdit extends Component {
     return listener
   }
 
-  setLocalState = (newState) => {
-    this.setState(newState)
+  setLocalState = (e) => {
+    e.preventDefault()
+    if (e.target.name === 'status') {
+      let userStatus={}
+      switch (e.target.value) {
+      case 'Going' :
+        userStatus = { id: '2', text: 'Going' }
+         console.log('in going user status', e.target.value)
+        break
+      case 'Invited' :
+        userStatus = { id: '1', text: 'Invited' }
+        break
+      case 'Can\'t make it' :
+        console.log('in cant make it target.value', e.target.value)
+        userStatus = { id: '3', text: 'Can\'t make it' }
+        console.log('in cant make it user status', e.target.value)
+        break
+      default :
+        console.log('in default', e.target.value)
+      }
+      this.setState({status: userStatus})
+    } else {
+      this.setState({[e.target.name]: e.target.value})
+    }
     // this.updateDb()
   }
 
   postUserInfoToDB = (e) => {
     e.preventDefault()
-    // console.log('FROM POST TO DB', this.state)
+    console.log('FROM POST TO DB', this.state)
     this.props.usersRef.child(this.props.userId)
       .update({
         name: this.state.name || 'Please enter your name',
@@ -94,6 +117,7 @@ export default class InlineBuddyEdit extends Component {
         startDate: this.validateDate(this.state.startDate),
         endDate: this.validateDate(this.state.endDate)
       })
+    document.getElementById('editYourInfoModal').style.display = 'none'
   }
 
   validateDate = (date) => {
@@ -117,70 +141,102 @@ export default class InlineBuddyEdit extends Component {
     // console.log('REF FROM RENDER', db.ref('/trips/'+ this.props.tripId + '/buddies').child(this.props.userId || 'test'))
     // console.log('REF FROM RENDER', this.props.tripRef.child('/buddies').child(this.props.userId || 'test'))
     return (
-      <form onSubmit={this.postUserInfoToDB}>
+      <form
+        onSubmit={this.postUserInfoToDB}
+        id="edit-user">
         <div className="container">
           <div className="form-horizontal">
-            <div>
-              <span>Name: </span>
-              <RIEInput
-                value={this.state.name}
-                change={this.setLocalState}
-                propName="name"
-                className={this.state.highlight ? "editable" : ""}
-                validate={this.isStringAcceptable}
-                classLoading="loading"
-                classInvalid="Invalid"
-                className="buddyEditFields" />
-            </div>
-            <br />
-            <div>
-              <span>Status: </span>
-              <RIESelect
-                value={this.state.status}
-                className={this.state.highlight ? "editable" : ""}
-                options={this.state.statusOptions}
-                change={this.setLocalState}
-                classLoading="loading"
-                propName="status" 
-                className="buddyEditFields"/>
-            </div>
-            <br />
-            <div>
-              <span>Home Base: </span>
-              <RIEInput
-                value={this.state.homeBase}
-                change={this.setLocalState}
-                propName="homeBase"
-                className={this.state.highlight ? "editable" : ""}
-                validate={this.isStringAcceptable}
-                classLoading="loading"
-                classInvalid="Invalid" 
-                className="buddyEditFields"/>
-            </div>
-            <br />
-            <div>
-              <span>Free from: </span>
-              <DatePicker
-                selected={this.state.startDate ? moment(this.state.startDate) : null}
-                onChange={this.handleChangeStart}
-                className="buddyEditFields"
-              />
-            </div>
-            <br />
-            <div>
-              <span>Free until: </span>
-              <DatePicker
-                selected={this.state.endDate ? moment(this.state.endDate) : null}
-                onChange={this.handleChangeEnd}
-                className="buddyEditFields"
-              />
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <span>Name: </span>
+                </td>
+                <td className="buddyEditFields">
+                </td>
+                <p>    </p>
+                <td>
+                  <input
+                    type="text"
+                    placeholder={this.state.name}
+                    onChange={this.setLocalState}
+                    className={this.state.highlight ? "editable" : ""}
+                    name="name"
+                    />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span>Status: </span>
+                </td>
+                <td className="buddyEditFields">
+                <p>    </p>
+                </td>
+                <td >
+                  <select
+                  onChange={this.setLocalState}
+                  name="status" >
+                    <option value="Invited">Invited</option>
+                    <option value="Going">Going</option>
+                    <option value="Can't make it">Can't make it</option>
+                  </select>
+                  </td>
+              </tr>
+              <tr>
+                <td>
+                  <span>Home Base: </span>
+                </td>
+                <td className="buddyEditFields">
+                <p>    </p>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    placeholder={this.state.homeBase}
+                    onChange={this.setLocalState}
+                    className={this.state.highlight ? "editable" : ""}
+                    name="homeBase"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span>Free from: </span>
+                  </td>
+                <td className="buddyEditFields">
+                <p>    </p>
+                </td>
+                  <td>
+                    <DatePicker
+                      selected={this.state.startDate ? moment(this.state.startDate) : null}
+                      onChange={this.handleChangeStart}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span>Free until: </span>
+                  </td>
+                <td className="buddyEditFields">
+                <p>    </p>
+                </td>
+                  <td>
+                    <DatePicker
+                      selected={this.state.endDate ? moment(this.state.endDate) : null}
+                      onChange={this.handleChangeEnd}
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
             </div>
           </div>
-        </div>
         <button
           className="modal-add-buddy-button"
           type="button"
-          className="btn btn-primary">Save Info</button>
+          className="btn btn-primary"
+          onClick={this.postUserInfoToDB}
+          form="edit-user">Save Info</button>
       </form>
     )
   }
