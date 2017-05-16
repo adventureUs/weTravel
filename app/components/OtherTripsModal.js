6470285937e4542ed7c7816425fafac5b0ef2443
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-05-16 09:09:55 -0400

    fleshed out listeners, db write, and html for trips modal

---
+++ app/components/OtherTripsModal.js
@@ -0,0 +1,132 @@
+import React from 'react'
+import { Link, browserHistory } from 'react-router'
+import firebase from 'APP/fire'
+const db = firebase.database()
+const auth = firebase.auth()
+import { RIEInput } from 'riek'
+import idToNameOrEmail from '../../src/idToNameOrEmail'
+import redirectToTripZeroeth from '../../src/redirectToTripZeroeth'
+
+export default class OtherTripsModal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newTripName: '',
+      tripsWithNames: null // intended format is:
+      // { tripId1: tripName1, tripId2: tripName2, ...}
+    }
+  }
+
+  componentDidMount() {
+    console.log('OTHER_TRIPS_MODAL ComponentWILLMOUNT,  PROPS', this.props)
+    this.getTripsArrayWithNames()
+  }
+  componentWillUnmount() {
+    console.log('OTHER_TRIPS_MODAL ComponentWILL_UNMOUNT')
+    this.unsubscribeTripsRef && this.unsubscribeTripsRef()
+    this.unsubscribeUserRef && this.unsubscribeUserRef()
+  }
+  getTripsArrayWithNames = () => {
+    if (this.unsubscribeUserRef) { this.unsubscribeUserRef() }
+    const userRefListener = this.props.userRef
+      .on('value', snapshot => {
+        console.log('OTHER_TRIPS_MODAL DID_MOUNT: userRefListener, snapshot', this.props.userRef, snapshot)
+        let userObj = null
+        if (snapshot) { userObj = snapshot.val() }
+        if (userObj) {
+          const trips = userObj.trips
+          this.triggerTripsRefListener(trips)
+        }
+      })
+    this.unsubscribeUserRef = () => this.props.userRef.off('value', userRefListener)
+  }
+  triggerTripsRefListener = (trips) => {
+    if (this.unsubscribeTripsRef) { this.unsubscribeTripsRef() }
+    const tripsRefListener = this.props.tripsRef
+      .on('value', snapshot => {
+        console.log('OTHER_TRIPS_MODAL DID_MOUNT: tripsRefListener, snapshot', this.props.tripsRef, snapshot)
+        let tripsObj = null
+        if (snapshot) { tripsObj = snapshot.val() }
+        this.updateTripsArrayWithNames(trips, tripsObj)
+      })
+    this.unsubscribeTripsRef = () => this.props.userRef.off('value', tripsRefListener)
+  }
+  updateTripsArrayWithNames = (trips, tripsObj) => {
+    const tripsWithNames = {}
+    // loop over the users tripIds and find the tripName
+    Object.keys(trips).forEach(key =>
+      tripsWithNames[key] = tripsObj[key].tripName
+    )
+    this.setState(tripsWithNames)
+  }
+
+  changeTrip = (e) => {
+    console.log('OTHER_TRIPS_MODAL, CHANGING TRIP...', 'CLICKED TRIP', e.traget)
+    // slice trips and splice the e.target.id to zeroeth index
+    this.props.userRef
+      .once('value')
+      .then(snapshot => {
+        let userObj = null
+        const targetTrip = e.target.id
+        if (snapshot) { userObj = snapshot.val() }
+        if (userObj) {
+          const newTrips = userObj.trips.slice()
+          newTrips.splice(newTrips.indexOf(targetTrip), 1)
+          newTrips.unshift(targetTrip)
+          console.log('OTHER_TRIPS_MODAL, CHANGING TRIP...', 'Old trips, new', userObj.trips, newTrips)
+          this.props.userRef
+            .update({ trips: newTrips })
+        }
+      })
+      .catch(console.error)
+    redirectToTripZeroeth(this.props.userId)
+  }
+
+  makeNewTrip = () => {
+  }
+    //   // Make a new trip with id, and add that id to currentUser.
+    //   // Set the new trip Id to currentTrip, trigger rerender of new Dashboard
+    //   console.log(document.getElementById('newTripInput').value)
+
+  render() {
+    console.log('STATE in TITLEBAR', this.state)
+    const tripsWithNames = this.state.tripsWithNames
+    return tripsWithNames ?
+      (
+      <div className="modal" id="tripsModal">
+        <div className="modal-dialog modal-sm">
+          <div className="modal-content">
+            <div className="modal-header">
+              <button type="button" className="close"
+                onClick={() =>
+                  document.getElementById('tripsModal').style.display = 'none'}
+              >&times;
+              </button>
+              <h4 className="modal-title">Your Trips</h4>
+            </div>
+            <div className="modal-body">
+              {(Object.keys(tripsWithNames).map((tripId) =>
+                <h4 id={`${tripId}`} key={`${tripId}`}
+                  onClick={this.changeTrip}
+                  style={{ border: 'bottom' }}
+                ><font color='#18bc9c'>{tripsWithNames[tripId]}</font></h4>))}
+            </div>
+            <div className="modal-footer"
+              style={{
+                display: 'flex',
+                justifyContent: 'space-around'
+              }}>
+              <input type="text" id="newTripInput"></input>
+              <button type="button" className="btn btn-primary"
+                onClick={this.makeNewTrip}>
+                Add a trip
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    :
+    null
+  }
+}
--- app/components/TitleBar.jsx
+++ app/components/TitleBar.jsx
@@ -3,8 +3,8 @@ import { Link, browserHistory } from 'react-router'
 import firebase from 'APP/fire'
 const db = firebase.database()
 const auth = firebase.auth()
-const trips = ['Rome', 'Montenegro']
 import { RIEInput } from 'riek'
+import OtherTripsModal from './OtherTripsModal'
 import idToNameOrEmail from '../../src/idToNameOrEmail'
 
 export default class TitleBar extends React.Component {
@@ -15,15 +15,14 @@ export default class TitleBar extends React.Component {
       userName: ''
     }
   }
-
   componentDidMount() {
     // console.log('TITLE BAR ComponentWILLMOUNT,  PROPS', this.props)
     this.unsubscribe = this.props.tripRef
     .on('value', snapshot => {
       // console.log('TITLE BAR DID_MOUNT: tripRef, snapshot', this.props.tripRef, snapshot)
-      // Stef says: Weird edge case on logout:  tripRef and snapshot log as existing
-      // but snapshot.val() finds snapshot undefined...
-      // safety (hack?) is the if below:
+      /* Stef says: Weird edge case on logout:  tripRef and snapshot log as existing
+       but snapshot.val() finds snapshot undefined...
+       safety (hack?) is the if below: */
       if (!snapshot) return function() {}
       const tripObj = snapshot.val()
       idToNameOrEmail(this.props.userId)
@@ -37,34 +36,16 @@ export default class TitleBar extends React.Component {
     // console.log('TITLE BAR ComponentWILL_UNMOUNT')
     this.unsubscribe()
   }
-
-  getAllTrips() {
-    // Get other trip name via currentUser's associated trip Ids
-  }
-
-  // changeTrip = (e) => {}
-    //   // Note: change map over trips to reflect actualy trip id and names.
-    //   // e.target.id set to currentTrip
-    //   browserHistory.push('/dashboard/'+this.props.tripId)
-
-  makeNewTrip() {
-  }
-    //   // Make a new trip with id, and add that id to currentUser.
-    //   // Set the new trip Id to currentTrip, trigger rerender of new Dashboard
-    //   console.log(document.getElementById('newTripInput').value)
-
   setStates = (newState) => {
     this.setState(newState)
     this.postTripNameToDB(newState.tripName)
   }
-
   postTripNameToDB = (tripName) => {
     this.props.tripsRef.child('/' + this.props.tripId)
       .update({
         tripName: tripName || 'New Trip Name',
       })
   }
-
   render() {
     // console.log('STATE in TITLEBAR', this.state)
     return this.state.tripName ?
@@ -102,6 +83,21 @@ export default class TitleBar extends React.Component {
                 className="titleBarTitle"
               />
             </h4>
+            <button style={{
+              color: '#18bc9c',
+              backgroundColor: '#ffffff',
+              borderRadius: '5px',
+              padding: '1px 6px'
+            }}
+              type="button"
+              onClick={() =>
+                document.getElementById('tripsModal').style.display = 'block'}
+              >+</button>
+            <OtherTripsModal
+              tripRef={this.props.tripRef}
+              tripsRef={this.props.tripsRef}
+              userId={this.props.userId}
+              />
           </div>
 
           <div style={{
@@ -147,49 +143,3 @@ export default class TitleBar extends React.Component {
     null
   }
 }
-//Bttn and Modal for All Trips:
-
-// <button style={{
-//               color: '#18bc9c',
-//               backgroundColor: '#ffffff',
-//               borderRadius: '5px',
-//               padding: '1px 6px'
-//             }}
-//               type="button"
-//               onClick={() =>
-//                 document.getElementById('tripsModal').style.display = 'block'}
-//             >+</button>
-
-//   <div className="modal" id="tripsModal">
-//     <div className="modal-dialog modal-sm">
-//       <div className="modal-content">
-//         <div className="modal-header">
-//           <button type="button" className="close"
-//             onClick={() =>
-//               document.getElementById('tripsModal').style.display = 'none'}
-//           >&times;
-//           </button>
-//           <h4 className="modal-title">Your Trips</h4>
-//         </div>
-//         <div className="modal-body">
-//           {(this.getAllTrips().map((trip, idx) =>
-//             <h4 id={`${idx}`} key={`${idx}`}
-//               onClick={this.changeTrip}
-//               style={{ border: 'bottom' }}
-//             ><font color='#18bc9c'>{trip}</font></h4>))}
-//         </div>
-//         <div className="modal-footer"
-//           style={{
-//             display: 'flex',
-//             justifyContent: 'space-around'
-//           }}>
-//           <input type="text" id="newTripInput"></input>
-//           <button type="button" className="btn btn-primary"
-//             onClick={this.makeNewTrip}>
-//             Add a trip
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
-// </div>
