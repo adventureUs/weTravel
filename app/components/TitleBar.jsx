f6a0fbffd194c0bace9f24dc6be2ff307d3733e5
Maria Schreiber <meschreiber3@gmail.com>
2017-05-17 15:52:24 -0400

    style changes

--- app/components/TitleBar.jsx
+++ app/components/TitleBar.jsx
@@ -137,22 +137,6 @@ export default class TitleBar extends React.Component {
                   ? `Welcome, ${this.state.userName}!`
                   : ''}</font>
               </h4>
-              <button style={{
-              color: '#18bc9c',
-              backgroundColor: '#ffffff',
-              borderRadius: '5px',
-              padding: '5px'
-            }}
-              type="button"
-              onClick={() =>
-                document.getElementById('other-trips-modal').style.display = 'block'}
-              >Trip List</button>
-            <OtherTripsModal
-              tripRef={this.props.tripRef}
-              tripsRef={this.props.tripsRef}
-              userId={this.props.userId}
-              userRef={this.props.userRef}
-              />
               {auth && auth.currentUser ?
                 <button className='logout'
                   style={{
